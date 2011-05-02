60f18d06db9de24102f9d75a5b93d9fdd7cb1aec
Michael Victor Zink <zuwiki@zuwiki.net>
2011-05-02 15:18:44 -0500

    Swizzling around some labels

--- RainbowDash/RDHudLayer.m
+++ RainbowDash/RDHudLayer.m
@@ -11,4 +11,14 @@
 
 @implementation RDHudLayer
 
+- (id) init {
+	if ((self = [super init])) {
+		CCLabelTTF * label = [CCLabelTTF labelWithString: @"Rainbow Dash 2D" fontName: @"Palatino-Roman" fontSize: 24.0];
+		CGSize size = [[CCDirector sharedDirector] winSize];
+		label.position = ccpAdd(ccp(label.contentSize.width / 2.0, size.height), ccp(20.0, -30.0));
+		[self addChild: label];
+	}
+	return self;
+}
+
 @end
--- RainbowDash/RDMainMenu.m
+++ RainbowDash/RDMainMenu.m
@@ -19,10 +19,16 @@
 
 - (id) init {
 	if ((self = [super init])) {
-		CCLabelTTF * label = [CCLabelTTF labelWithString: @"Rainbow Dash 2D" fontName: @"Palatino-Roman" fontSize: 24.0];
 		CGSize size = [[CCDirector sharedDirector] winSize];
-		label.position = ccpAdd(ccp(label.contentSize.width / 2.0, size.height), ccp(20.0, -30.0));
+		CGPoint center = ccp(size.width / 2.0, size.height / 2.0);
+
+		CCLabelTTF * label = [CCLabelTTF labelWithString: @"Rainbow Dash 2D" fontName: @"Palatino-Roman" fontSize: 44.0];
+		label.position = ccpAdd(center, ccp(0.0, 40.0));
 		[self addChild: label];
+
+		CCLabelTTF * button = [CCLabelTTF labelWithString: @"Start" fontName: @"Palatino-Roman" fontSize: 34.0];
+		button.position = ccpSub(center, ccp(0.0, 50.0));
+		[self addChild: button];
 	}
 	return self;
 }
